dc604677ae7bbba94ee0d5d726fba7fbac5c27c7
RuixeWolf <RuixeWolf@outlook.com>
2025-07-28 22:41:34 +0800

    init

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { zh_cn } from '@nuxt/ui/locale'
+</script>
+
+<template>
+  <UApp :locale="zh_cn">
+    <div class="min-h-screen min-w-screen">
+      <NuxtPage />
+    </div>
+  </UApp>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="p-10 text-center">
+    <h1>Welcome to Wolf Blog</h1>
+    <UButton class="mt-5">Nuxt UI Test</UButton>
+  </div>
+</template>
+<style></style>
